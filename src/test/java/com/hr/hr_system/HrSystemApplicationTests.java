0ade551cf58e93d684760846dfd897e1c866af0f
rgogi <rgogi20@freeuni.edu.ge>
2023-09-02 13:51:19 +0400

    First commit

---
+++ src/test/java/com/hr/hr_system/HrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.hr_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
